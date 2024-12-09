5aaa09516211f8524b42bdb16c291b5d690e6041
Greham-Solanki <grehamsolanki91@gmail.com>
2024-12-09 18:28:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def home():
     return "Hello from Solanki ECS Container."
 
+@app.route('/health')  # Add a specific health check endpoint
+def health():
+    return "Healthy", 200
+    
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
